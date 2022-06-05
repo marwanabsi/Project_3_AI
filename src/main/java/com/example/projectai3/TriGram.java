ee91da848ad1bee1d6c012d210738b607f42f402
marwanabsi <marwanabsi98@gmail.com>
2022-06-05 18:02:03 +0300

    go go

---
+++ src/main/java/com/example/projectai3/TriGram.java
@@ -0,0 +1,11 @@
+package com.example.projectai3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class TriGram {
+    ArrayList<Type> tokens;
+    HashMap<String, Integer> triGramModel = new HashMap<String, Integer>();
+    HashMap triModel;
+
+}
--- src/main/java/com/example/projectai3/Type.java
+++ src/main/java/com/example/projectai3/Type.java
@@ -4,4 +4,32 @@ public class Type {
     private String word;
     private int Freq;
 
+    public Type(String word, int freq) {
+        this.word = word;
+        Freq = freq;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getFreq() {
+        return Freq;
+    }
+
+    public void setFreq(int freq) {
+        Freq = freq;
+    }
+
+    @Override
+    public String toString() {
+        return "Type{" +
+                "word='" + word + '\'' +
+                ", Freq=" + Freq +
+                '}';
+    }
 }
